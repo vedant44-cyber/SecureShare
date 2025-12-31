3fef1974794ed5e5726ea0303e6ec965b529e7f0
Benelux <vedantdorlikar44@gmail.com>
2026-01-01 01:39:59 +0530

    update home handler

--- backend/internal/handlers/root.go
+++ backend/internal/handlers/root.go
@@ -3,5 +3,5 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func (depends *HandlerDependencies) HandleRoot(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "oedsadsfadfk"})
+	c.JSON(200, gin.H{"message": "Welcome to the Secure Share API"})
 }
